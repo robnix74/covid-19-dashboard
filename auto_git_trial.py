55072f3e3ed8b56e6ce2bfa46b48fe01ad256bf3
Robert Nixon T <robertnixon99@gmail.com>
2020-05-04 16:54:37 +0530

    'auto-update'

---
+++ auto_git_trial.py
@@ -0,0 +1,15 @@
+import subprocess as cmd
+
+cp = cmd.run("git add .", check=True, shell=True)
+print(cp)
+
+# response = input("Do you want to use the default message for this commit?([y]/n)\n")
+# message = "auto-update"
+
+# if response.startswith('n'):
+#     message = input("Type commit message\n")
+
+message = 'auto-update'
+
+cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+cp = cmd.run("git push -u covid-19-dashboard master -f", check=True, shell=True)
